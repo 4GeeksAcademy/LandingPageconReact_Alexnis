56610710a6cf6c9234fe06cf05bf7d57377f4e33
Alexnis-Herrera <alexnisherrerag@gmail.com>
2025-07-07 11:02:20 +0000

    enproceso

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,10 @@
+export const Jumbotron = () => {
+    return (
+        <div>
+            <h1>A warm welcome</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut corporis, dicta quae vero optio alias ipsam, quas harum sed suscipit facilis tenetur nisi dolor, ipsa dolorum delectus sint libero ipsum.</p>
+            <button>call to action</button>
+        </div>
+    )
+}
+
